6f4cfeba7afe7950f873d570922bbb1e43257fe1
CrystalNoob <69108822+CrystalNoob@users.noreply.github.com>
2024-05-04 23:56:32 +0700

    feat: updated dictionary

--- src/Dictionary/Dictionary.java
+++ src/Dictionary/Dictionary.java
@@ -3,7 +3,9 @@ package Dictionary;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Dictionary {
@@ -18,5 +20,22 @@ public class Dictionary {
 			System.err.println("Error while reading file: " + e.getMessage());
 		}
 		return wordset;
+	}
+
+	public static List<String> getNeighbors(String word, Set<String> wordSet) {
+		List<String> neighbors = new ArrayList<>();
+		char[] charsOfWord = word.toCharArray();
+		for(int i = 0; i < charsOfWord.length; i++) {
+			char originalChar = charsOfWord[i];
+			for(char c = 'a'; c <= 'z'; c++) {
+				charsOfWord[i] = c;
+				String newWord = new String(charsOfWord);
+				if(wordSet.contains(newWord) && !newWord.equals(word)) {
+					neighbors.add(newWord);
+				}
+			}
+			charsOfWord[i] = originalChar;
+		}
+		return neighbors;
 	}
 }
