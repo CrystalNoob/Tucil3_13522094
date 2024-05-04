d1374260bcebada6bdc73abe91df1e94f535a958
CrystalNoob <69108822+CrystalNoob@users.noreply.github.com>
2024-05-04 23:55:32 +0700

    feat: addded CLI

---
+++ src/CLI/CLI.java
@@ -0,0 +1,46 @@
+package CLI;
+
+import java.util.List;
+import java.util.Set;
+import Algorithms.Astar;
+import Algorithms.GBFS;
+import Algorithms.UCS;
+import Dictionary.Dictionary;
+
+public class CLI {
+	public void start(String[] args) throws Exception {
+		String algorithm = args[0].toLowerCase();
+        String filePath = args[1];
+		String startWord = args[2].toLowerCase();
+		String endWord = args[3].toLowerCase();
+        Set<String> dictionary = Dictionary.Wordset(filePath);
+
+		List<String> path;
+		long startTime, endTime;
+
+		startTime = System.nanoTime();
+
+		switch (algorithm) {
+		case "astar":
+			path = Astar.findPath(startWord, endWord, dictionary);
+			break;
+		case "ucs":
+			path = UCS.findPath(startWord, endWord, dictionary);
+			break;
+        case "gbfs":
+            path = GBFS.findPath(startWord, endWord, dictionary);
+            break;
+		default:
+			throw new Exception("Invalid algorithm name");
+		}
+
+		endTime = System.nanoTime();
+
+        if (path != null) {
+            System.out.println("Shortest path from " + startWord + " to " + endWord + ": " + path);
+        } else {
+            System.out.println("No path found from " + startWord + " to " + endWord);
+        }
+		System.out.println("Time Elapsed: "+ (endTime - startTime)/1000000 + " ms");
+	}
+}
