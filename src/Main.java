61b5077a6a5856b8079cad679b07a1a30fd3af11
CrystalNoob <69108822+CrystalNoob@users.noreply.github.com>
2024-05-05 22:08:16 +0700

    fix: improved main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 import CLI.CLI;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-        CLI c = new CLI();
-        if (args.length < 4) {
-            System.out.println("Usage: Main <algorithm> <PathToDictionary> <startWord> <goalWord>");
-            System.exit(1);;
-        }
-        c.start(args);
-    }
+	public static void main(String[] args) {
+		if (args.length < 4) {
+			System.out.println("Usage: Main <algorithm> <PathToDictionary> <startWord> <goalWord>");
+			System.exit(1);;
+		}
+
+		try {
+			CLI c = new CLI();
+			c.start(args);
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
 }
